31fa46755c8a07be3f5c5ad18744283f51a0a8af
DevJoshi030 <47146845+DevJoshi030@users.noreply.github.com>
2020-01-27 19:47:05 +0530

    Assignment - I

---
+++ confusion/src/components/DishDetailsComponent.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetails extends Component {
+
+    constructor(props) {
+        super(props);
+        this.dish = null;
+    }
+
+    render() {
+
+        this.dish = this.props.dish;
+        if (this.dish != null) {
+            return (
+                <div className="row">
+                    <div className="col-md-5 m-1">
+                        <Card>
+                            <CardImg width='100%' src={ this.dish.image } alt={ this.dish.name } />
+                            <CardBody>
+                                <CardTitle>
+                                    { this.dish.name }
+                                </CardTitle>
+                                <CardText>
+                                    { this.dish.description }
+                                </CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    <div className="col-12 col-md">
+                        <h1>Comments:</h1>
+                        { this.dish.comments.map((comment) => {
+                            return (
+                                <div className="row">
+                                    <div className="col-12 col-md m-1">
+                                        <h4>{ comment.comment }</h4>
+                                        <br />
+                                        <h5>-- { comment.author }, { comment.date }</h5>
+                                        <br />
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div></div>
+            );
+        }
+    }
+
+}
+
+export default DishDetails;
--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import DishDetails from './DishDetailsComponent';
 
 class Menu extends Component {
 
@@ -14,29 +15,6 @@ class Menu extends Component {
         this.setState({
             selectedDish: dish
         });
-    }
-
-    renderDish(dish) {
-        if (dish != null) {
-            return (
-                <Card>
-                    <CardImg width='100%' src={ dish.image } alt={ dish.name } />
-                    <CardBody>
-                        <CardTitle>
-                            { dish.name }
-                        </CardTitle>
-                        <CardText>
-                            { dish.description }
-                        </CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        else {
-            return (
-                <div></div>
-            );
-        }
     }
 
     render() {
@@ -59,9 +37,7 @@ class Menu extends Component {
                 <div className='row'>
                     { menu }
                 </div>
-                <div className='row'>
-                    { this.renderDish(this.state.selectedDish) }
-                </div>
+                <DishDetails dish={ this.state.selectedDish }></DishDetails>
             </div>
         );
     }
