3e942eae609ab211a53621da310af963499dbd02
DevJoshi030 <47146845+DevJoshi030@users.noreply.github.com>
2020-01-27 20:18:11 +0530

    Presentational and Container Components

--- confusion/src/components/DishDetailsComponent.js
+++ confusion/src/components/DishDetailsComponent.js
@@ -13,34 +13,36 @@ class DishDetails extends Component {
         this.dish = this.props.dish;
         if (this.dish != null) {
             return (
-                <div className="row">
-                    <div className="col-md-5 m-1">
-                        <Card>
-                            <CardImg width='100%' src={ this.dish.image } alt={ this.dish.name } />
-                            <CardBody>
-                                <CardTitle>
-                                    { this.dish.name }
-                                </CardTitle>
-                                <CardText>
-                                    { this.dish.description }
-                                </CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    <div className="col-12 col-md">
-                        <h1>Comments:</h1>
-                        { this.dish.comments.map((comment) => {
-                            return (
-                                <div className="row">
-                                    <div className="col-12 col-md m-1">
-                                        <h4>{ comment.comment }</h4>
-                                        <br />
-                                        <h5>-- { comment.author }, { comment.date }</h5>
-                                        <br />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-5 m-1">
+                            <Card>
+                                <CardImg width='100%' src={ this.dish.image } alt={ this.dish.name } />
+                                <CardBody>
+                                    <CardTitle>
+                                        { this.dish.name }
+                                    </CardTitle>
+                                    <CardText>
+                                        { this.dish.description }
+                                    </CardText>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        <div className="col-12 col-md">
+                            <h1>Comments:</h1>
+                            { this.dish.comments.map((comment) => {
+                                return (
+                                    <div className="row">
+                                        <div className="col-12 col-md m-1">
+                                            <h4>{ comment.comment }</h4>
+                                            <br />
+                                            <h5>-- { comment.author }, { new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date))) }</h5>
+                                            <br />
+                                        </div>
                                     </div>
-                                </div>
-                            );
-                        })}
+                                );
+                            })}
+                        </div>
                     </div>
                 </div>
             );
