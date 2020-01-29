12f776a4cfe7e574e49d64a99931d240a42c039d
DevJoshi030 <47146845+DevJoshi030@users.noreply.github.com>
2020-01-29 19:26:38 +0530

    Build Upload

---
+++ confusion/build/precache-manifest.56b57478cdb772c29066926ed998415e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5754e245ecd836cfb2f29cc7bfdc1a10",
+    "url": "/First_React_App/index.html"
+  },
+  {
+    "revision": "ec3735f1ba93fb4ce89d",
+    "url": "/First_React_App/static/css/2.2e480f3d.chunk.css"
+  },
+  {
+    "revision": "9645b1da3520f9d5d249",
+    "url": "/First_React_App/static/css/main.64c871f9.chunk.css"
+  },
+  {
+    "revision": "ec3735f1ba93fb4ce89d",
+    "url": "/First_React_App/static/js/2.dc8c8304.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/First_React_App/static/js/2.dc8c8304.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9645b1da3520f9d5d249",
+    "url": "/First_React_App/static/js/main.62ba3a42.chunk.js"
+  },
+  {
+    "revision": "4dcf47333f14f2e76ff8",
+    "url": "/First_React_App/static/js/runtime-main.5a96414b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/First_React_App/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/First_React_App/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/First_React_App/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/First_React_App/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/First_React_App/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
